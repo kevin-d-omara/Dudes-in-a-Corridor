0059a51523b88a43308091a4dd5fa7f8b56db97b
kevin-d-omara <kevin.d.omara@gmail.com>
2016-07-31 19:22:10 -0700

    reduced redundancy

--- tests/Cell/test_Cell.lua
+++ tests/Cell/test_Cell.lua
@@ -3,19 +3,21 @@ require 'Cell'
 TestCell_Instantiation = {}
     function TestCell_Instantiation:test_argument_emptyTable()
         local cell = Cell:new{}
+        luaunit.assertEquals(cell.blocksMove, true)
+        luaunit.assertEquals(cell.blocksSight, true)
+        luaunit.assertEquals(cell.blocksAttack, true)        
     end
     
     function TestCell_Instantiation:test_argument_none()
-        local cell = Cell:new()        
+        local cell = Cell:new()
+        luaunit.assertEquals(cell.blocksMove, true)
+        luaunit.assertEquals(cell.blocksSight, true)
+        luaunit.assertEquals(cell.blocksAttack, true)  
         luaunit.assertEquals(args, nil) -- confirm newly created 'args' is local
     end
     
     function TestCell_Instantiation:test_booleans_trueFalse()
         local cell = Cell:new{blocksMove = true}        -- blocksMove
-        luaunit.assertEquals(cell.blocksMove, true)
-        local cell = Cell:new{}
-        luaunit.assertEquals(cell.blocksMove, true)
-        local cell = Cell:new()
         luaunit.assertEquals(cell.blocksMove, true)
         local cell = Cell:new{blocksMove = false}
         luaunit.assertEquals(cell.blocksMove, false)
@@ -23,14 +25,10 @@ TestCell_Instantiation = {}
         
         local cell = Cell:new{blocksSight = true}       -- blocksSight
         luaunit.assertEquals(cell.blocksSight, true)
-        local cell = Cell:new{}
-        luaunit.assertEquals(cell.blocksSight, true)
         local cell = Cell:new{blocksSight = false}
         luaunit.assertEquals(cell.blocksSight, false)
         
         local cell = Cell:new{blocksAttack = true}      -- blocksAttack
-        luaunit.assertEquals(cell.blocksAttack, true)
-        local cell = Cell:new{}
         luaunit.assertEquals(cell.blocksAttack, true)
         local cell = Cell:new{blocksAttack = false}
         luaunit.assertEquals(cell.blocksAttack, false)
