aff8a1ce24e85d9bcaa7b487512a3e2b59f3e8f0
kevin-d-omara <kevin.d.omara@gmail.com>
2016-07-31 22:35:16 -0700

    added unfinished function makeFrom(file)

--- Grid.lua
+++ Grid.lua
@@ -1,14 +1,21 @@
 --[[
 Class - Grid
 Description:
-    A grid is a rectangular collection of cells (i.e. 2d array).
+    A Grid is a rectangular collection of cells (i.e. 2d array).  Its cartesian
+    coordinates (x,y) have their origin at the bottom left.
+    i.e.  (1,2) (2,2)         (1,1) (1,2)
+          (1,1) (2,1)   NOT   (2,1) (2,2)
 Hierarchy:
     Ancestors: none
     Children:  none
 Fields:
+    - integer lenX; horizontal width of grid
+    - integer lenY; vertical width of grid
+Functions:
     - 
 --]]
 require 'Cell'
+require 'file_intput_output'    -- TODO: make local? test locality
 
 Grid = {}
 function Grid:new(args)
@@ -32,5 +39,16 @@ function Grid:new(args)
     return obj
 end
 
--- define public interface
--- TODO
+
+function Grid:makeFrom(file)
+    local lines = linesFrom(file)
+
+    for i,v in ipairs(lines) do
+      print('line[' .. i .. ']', v)
+    end
+    
+    --http://stackoverflow.com/questions/19262761/lua-need-to-split-at-comma
+end
+
+Grid:makeFrom('dummyfile.txt')
+-- TODO: define public interface
