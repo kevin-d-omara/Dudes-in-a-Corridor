6473b4f7419153464b9e862e9889e80b13bd5db8
kevin-d-omara <kevin.d.omara@gmail.com>
2016-08-05 12:03:24 -0700

    formatted comment header; + function prototype

--- Cell.lua
+++ Cell.lua
@@ -7,15 +7,19 @@ Hierarchy:
     Ancestors: none
     Children:  none
 Fields:
-    - boolean blocksMove, blocksSight, blocks
+    - boolean  :: blocksMove, blocksSight, blocksAttack
+    - table {} :: intrinsic
+        - boolean :: blocksMove, blocksSight, blocks
       TODO need duplicate fields for square itself (i.e. wall vs token)
+Public API
+    - boolean  :: blocksMove, blocksSight, blocksAttack
 --]]
 
 Cell = {}
 function Cell:new(args)
     local obj = {}
     
-    -- default Cell is a wall with no contained elements
+    -- default Cell is a 'wall' with no contained elements
     if args == nil then args = {} end
     obj.blocksMove = (args.blocksMove == nil) and true or args.blocksMove
     obj.blocksSight = (args.blocksSight == nil) and true or args.blocksSight
@@ -24,14 +28,18 @@ function Cell:new(args)
     setmetatable(obj, self)
     self.__index = self
     --table.insert(Cell.allCells, obj)
+    obj:updateBooleans()
     return obj
 end
 
---[[ Define public API
-boolean Cell.blocksMove
-boolean Cell.blocksSight
-boolean Cell.blocksAttack
-It is the Cell's job to keep these updated, so that raycasts/LoS checks are as
-fast as possible.  i.e. when a Token/Unit, etc. is inserted/removed, these
-values should be updated.
+--[[
+- Keeps 'ray interaction' booleans up to date so that the raycasting algorithm
+  is as fast as possible.
+- Must be called whenever the Cell's state changes.
 --]]
+function Cell:updateBooleans()
+    -- Each public value is the 'logical or' of all its component values
+    -- If any component blocks, then the public value is 'true'
+    
+    -- TODO: implement + test
+end
