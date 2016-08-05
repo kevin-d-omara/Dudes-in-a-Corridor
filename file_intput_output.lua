7e861900ab2fbea5b8a06f486c21be55cedd383d
kevin-d-omara <kevin.d.omara@gmail.com>
2016-08-05 12:51:04 -0700

    ++function descriptors

--- file_intput_output.lua
+++ file_intput_output.lua
@@ -11,14 +11,14 @@ http://www.lua.org/manual/5.2/manual.html#6.4.1
 http://lua-users.org/wiki/StringLibraryTutorial
 --]]
 
--- see if the file exists
+-- Return true if file exists and is readable, false otherwise.
 function fileExists(file)
   local f = io.open(file, "rb")
   if f then f:close() end
   return f ~= nil
 end
 
--- get all lines from a file; returns an empty table if file is non existant
+-- Get all lines from a file.  Returns an empty table if file is non existant.
 function linesFrom(file)
   if not fileExists(file) then return {} end
   lines = {}
