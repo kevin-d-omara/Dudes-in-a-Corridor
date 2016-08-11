f5cd78e351002d934673e19db27c84c0601e0b78
kevin-d-omara <kevin.d.omara@gmail.com>
2016-08-11 10:29:28 -0700

    contains user-immutable settings

--- conf.lua
+++ conf.lua
@@ -1,14 +1,18 @@
-function love.conf(t)
-    t.window.title = "Dudes in a Corridor" -- The window title (string)
-    t.window.width = 1024               -- The window width (number)
-    t.window.height = 768               -- The window height (number)
-    
 --[[
-    t.window.borderless = false         -- Remove all border visuals from the window (boolean)
+Description:
+    - Contains immutable settings for the game.
+--]]
+function love.conf(t)
+    t.version = "0.10.1"                -- The LÖVE version this game was made for (string)
+    t.window.title = "Dudes in a Corridor" -- The window title (string)
+
+--[[
     t.window.resizable = false          -- Let the window be user-resizable (boolean)
     t.window.minwidth = 1               -- Minimum window width if the window is resizable (number)
     t.window.minheight = 1              -- Minimum window height if the window is resizable (number)
+    t.window.fullscreen = false         -- Enable fullscreen (boolean)
+    t.window.fullscreentype = "desktop" -- Choose between "desktop" fullscreen or "exclusive" fullscreen mode (string)
     t.window.x = nil                    -- The x-coordinate of the window's position in the specified display (number)
     t.window.y = nil                    -- The y-coordinate of the window's position in the specified display (number)
- --]]
+--]]
  end
