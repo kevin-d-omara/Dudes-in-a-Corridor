b9f6c9e6ce3d9d9c841596670cc1c85b4902b8f5
kevin-d-omara <kevin.d.omara@gmail.com>
2016-08-11 10:29:42 -0700

    applies user 'settings.lua'

--- initialize.lua
+++ initialize.lua
@@ -1,6 +1,31 @@
+local ini = require 'settings'
+
+-- Apply Window Settings
+love.window.setMode(ini.width, ini.height, ini.flags)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+--[[ future reference material
 math.randomseed( tonumber(tostring(os.time()):reverse():sub(1,6)) )
 --rng = love.math.newRandomGenerator() -- see API online
 --love.math.random() -- same seed across platforms?
-
-window = {}
-window.width, window.height, window.flags = love.window.getMode()
+--]]
